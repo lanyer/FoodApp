9910eddbc0a61634e2f2d69252c5127522d200ac
lanyer <670507813@qq.com>
2016-08-20 15:43:21 +0800

    Establish 3 projects about Layers

---
+++ BusinessLayer/BusinessLayer/BusinessLayer.h
@@ -0,0 +1,19 @@
+//
+//  BusinessLayer.h
+//  BusinessLayer
+//
+//  Created by lanyer on 16/8/20.
+//  Copyright (c) 2016年 lanyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BusinessLayer.
+FOUNDATION_EXPORT double BusinessLayerVersionNumber;
+
+//! Project version string for BusinessLayer.
+FOUNDATION_EXPORT const unsigned char BusinessLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BusinessLayer/PublicHeader.h>
+
+
---
+++ PersistenceLayer/PersistenceLayer/PersistenceLayer.h
@@ -0,0 +1,19 @@
+//
+//  PersistenceLayer.h
+//  PersistenceLayer
+//
+//  Created by lanyer on 16/8/20.
+//  Copyright (c) 2016年 lanyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PersistenceLayer.
+FOUNDATION_EXPORT double PersistenceLayerVersionNumber;
+
+//! Project version string for PersistenceLayer.
+FOUNDATION_EXPORT const unsigned char PersistenceLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PersistenceLayer/PublicHeader.h>
+
+
