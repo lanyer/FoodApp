31863bbf66d07a70e5634bf2ccbe9cee900b5701
lanyer <670507813@qq.com>
2016-08-20 15:52:06 +0800

    对表示层的修改

---
+++ PresentationLayer/PresentationLayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PresentationLayer
+//
+//  Created by lanyer on 16/8/20.
+//  Copyright (c) 2016年 lanyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PresentationLayer/PresentationLayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PresentationLayer
+//
+//  Created by lanyer on 16/8/20.
+//  Copyright (c) 2016年 lanyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
